2000ce679e5ce7f380dd5900dd7b7dd78bd0562c
312-KimJungHyun <112wjdugs@gmail.com>
2024-12-26 10:07:10 +0900

    클래스, 모듈, 패키지 연습용

---
+++ 이론과정/game/__init__.py
@@ -0,0 +1,14 @@
+from .graphic.render import render_test
+
+VERSION = "3.11.2"
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+    print("게임 제작자 : 김기원")
+    print("게임 제작자 메일 : damin@naver.com")
+    print("게임 제작의뢰 : 010-111-2222")
+
+
+# 패키지 실행 초기화 문구
+print("Initializing game...")
+print("Loading...")
---
+++ 이론과정/game/graphic/render.py
@@ -0,0 +1,6 @@
+from game.sound.echo import echo_test
+#같은 선상에 있는 디렉토리 가지고 오는 법
+
+def render_test():
+    print("render_test() 실행")
+    echo_test()
